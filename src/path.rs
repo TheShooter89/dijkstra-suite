f027214e9749fa8b1f2d6b51177e5a40c2a7ee37
Francesco Paoletti <paoletti.francesco@gmail.com>
2026-06-11 13:26:35 +0200

    add basic path module documentation

--- src/path.rs
+++ src/path.rs
@@ -1,3 +1,30 @@
+//! Standardized path types and behaviours
+//!
+//! ## The`Path` type
+//!
+//! The `Path` type is the standardized way to represent a path across `dijkstra_suite`
+//! and the rest of the world
+//!
+//! A `Path` is represented by two properties, `weight` and `steps`
+//!
+//! The `weight` props holds the total weight (or, cost) of the path
+//!
+//! The `steps` prop holds an ordered list of node ids that represent the actual path
+//!
+//! ```rust
+//! use dijkstra_suite::path::Path;
+//!
+//! let mut path: Path<(i32, i32), i32> = Path::default();
+//! println!("path: {:?}", path);
+//!
+//! path.steps.push((4, 2));
+//! println!("path.weight: {:?}", path.weight);
+//! println!("path.steps: {:?}", path.steps);
+//!
+//! let expected_steps = vec![(4, 2)];
+//! assert_eq!(path.steps, expected_steps);
+//! ```
+
 use crate::node::{NodeId, NodeWeight};
 
 #[derive(Debug, Default)]
