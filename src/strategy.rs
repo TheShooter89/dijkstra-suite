5d6be7b193ac9a2747b54d49fbd690ba151c3f44
Francesco Paoletti <paoletti.francesco@gmail.com>
2026-07-03 13:35:52 +0200

    add Strategy struct documentation

--- src/strategy.rs
+++ src/strategy.rs
@@ -88,6 +88,59 @@ pub trait StrategyRunner {
     }
 }
 
+/// execute implementation strategies
+///
+/// ## `Strategy` type
+///
+/// The `Strategy` type provides static methods to run a strategy, with default or customized options
+///
+/// ### `execute()` method
+///
+/// This method runs a specific implementation strategy without specifiy any options,
+/// the method automatically calls `SelectedStrategy::Opts::default()` under the hood
+///
+/// ### `execute_with_opts()` method
+///
+/// This method runs exactly as `execute()` with the only difference that you must also
+/// specify the options for the choosen implementation strategy. Full control.
+///
+/// The `StrategyRunner` is a convenience trait, that automatically implements a `strategy()`
+/// method that runs `Strategy::execute_with_opts()` under the hood
+///
+/// ## Example usage
+///
+/// ```rust
+/// use dijkstra_suite::{
+///     graph::Graph,
+///     node::{NodeId, NodeWeight},
+///     path::Path,
+///     strategy::{ImplementationStrategy, Strategy}
+/// };
+///
+/// #[derive(Debug)]
+/// struct TestStrategy {}
+///
+/// impl ImplementationStrategy for TestStrategy {
+///     type Opts = ();
+///
+///     fn run<I: NodeId, W: NodeWeight>(
+///         graph: &Graph<I, W>,
+///         from: I,
+///         to: I,
+///         options: Self::Opts,
+///     ) -> Result<Path<I, W>, String> {
+///         Ok(Path::default())
+///     }
+/// }
+///
+/// let path = Strategy::execute::<TestStrategy, i32, i32>(&Graph::default(), 0, 99);
+///
+/// assert_eq!(path, Ok(Path::default()));
+///
+/// let path = Strategy::execute_with_opts::<TestStrategy, i32, i32>(&Graph::default(), 0, 99, ());
+///
+/// assert_eq!(path, Ok(Path::default()))
+/// ```
 #[derive(Debug)]
 pub struct Strategy {}
 
