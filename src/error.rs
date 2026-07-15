8e878300f996d72b055b04571286a40021e1d934
Francesco Paoletti <paoletti.francesco@gmail.com>
2026-07-15 20:50:59 +0200

    add error documentation and doctests

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,60 @@
+//! customized library-wide error handling
+//!
+//! ## The`DijkstraError` type
+//!
+//! The `DijkstraError` enum is a centralized error type, every library
+//! component which may return an error uses [`DijkstraError`]
+//!
+//! This type implements the [`Error`] trait manually, so to avoid introducing
+//! a dependency
+//!
+//! ```rust
+//! use dijkstra_suite::error::DijkstraError;
+//! use dijkstra_suite::compute::DistanceFromSource;
+//!
+//! let mut distances = DistanceFromSource::default();
+//!
+//! distances.set_distance((0, 0), 0, None);
+//! let node_error = DijkstraError::ComputePathError("end node not found".into());
+//!
+//! let result = distances.compute_path((0, 2));
+//!
+//! assert_eq!(result, Err(node_error));
+//! ```
+
 use std::{error::Error, fmt::Display};
 
+/// centralized error type
+///
+/// ## The `DijkstraError` error type
+///
+/// This enum is a centralized error type used throughout the library
+/// It implements the [`Error`] trait manually so to avoid an external
+/// dependency
+///
+/// ## Example
+///
+/// Main function [`dijkstra_path()`](crate::dijkstra::dijkstra_path)
+/// as well as internals like [`DistanceFromSource::compute_path()`](crate::compute::DistanceFromSource::compute_path)
+/// uses `DijkstraError`:
+///
+/// ```rust
+/// use dijkstra_suite::error::DijkstraError;
+/// use dijkstra_suite::compute::DistanceFromSource;
+///
+/// let mut distances = DistanceFromSource::default();
+///
+/// distances.set_distance((0, 0), 0, None);
+/// let node_error = DijkstraError::ComputePathError("end node not found".into());
+///
+/// let result = distances.compute_path((0, 2));
+///
+/// assert_eq!(result, Err(node_error));
+/// ```
 #[non_exhaustive]
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum DijkstraError {
+    /// error while generating the generic zero
     GenericZeroError(String),
     DistanceEntryNotFound(String),
     ComputePathError(String),
